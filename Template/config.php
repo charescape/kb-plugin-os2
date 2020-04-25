bed319dc5a8db30ef90dc6d4f87306a5fc7d85d8
charescape <charescape@outlook.com>
2020-04-25 18:18:22 +0800

    Update config.php

--- Template/config.php
+++ Template/config.php
@@ -1,27 +1,21 @@
 <h3>
-    <i class="fa fa-amazon fa-fw" aria-hidden="true"></i>
-    Amazon S3 Storage
+    <i class="fa fa-cloud" aria-hidden="true"></i>
+    Aliyun Object Storage Service
 </h3>
 <div class="panel">
-    <?= $this->form->label(t('AWS S3 region'), 'aws_s3_region') ?>
-    <?= $this->form->text('aws_s3_region', $values) ?>
+    <?= $this->form->label(t('Aliyun OSS endpoint'), 'aliyun_oss_endpoint') ?>
+    <?= $this->form->text('aliyun_oss_endpoint', $values) ?>
 
-    <?= $this->form->label(t('AWS S3 bucket'), 'aws_s3_bucket') ?>
-    <?= $this->form->text('aws_s3_bucket', $values) ?>
+    <?= $this->form->label(t('Aliyun OSS Bucket'), 'aliyun_oss_bucket') ?>
+    <?= $this->form->text('aliyun_oss_bucket', $values) ?>
 
-    <?= $this->form->label(t('AWS S3 objects prefix'), 'aws_s3_prefix') ?>
-    <?= $this->form->text('aws_s3_prefix', $values) ?>
+    <?= $this->form->label(t('Aliyun Access Key ID'), 'aliyun_oss_akey_id') ?>
+    <?= $this->form->password('aliyun_oss_akey_id', $values) ?>
 
-    <?= $this->form->label(t('AWS Access Key ID'), 'aws_access_key_id') ?>
-    <?= $this->form->password('aws_access_key_id', $values) ?>
+    <?= $this->form->label(t('Aliyun Access Key Secret'), 'aliyun_oss_akey_secret') ?>
+    <?= $this->form->password('aliyun_oss_akey_secret', $values) ?>
 
-    <?= $this->form->label(t('AWS Secret Key'), 'aws_secret_access_key') ?>
-    <?= $this->form->password('aws_secret_access_key', $values) ?>
-
-    <?= $this->form->label(t('AWS S3 custom options'), 'aws_s3_options') ?>
-    <?= $this->form->text('aws_s3_options', $values) ?>
-
-    <p class="form-help"><a href="https://github.com/kanboard/plugin-s3#configuration" target="_blank"><?= t('Help on Amazon S3 integration') ?></a></p>
+    <p class="form-help"><a href="https://github.com/charescape/kb-plugin-os2" target="_blank"><?= t('Help on Aliyun Object Storage Service integration') ?></a></p>
 
     <div class="form-actions">
         <button class="btn btn-blue"><?= t('Save') ?></button>
